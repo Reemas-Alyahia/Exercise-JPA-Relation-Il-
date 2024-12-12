942e59bc96c61e52e2066747815552258dfe30df
Reemas Alyahia <130400514+Reemas-Alyahia@users.noreply.github.com>
2024-12-12 13:56:18 +0300

    Add files via upload

---
+++ java/com/example/jpa121/Model/Course.java
@@ -0,0 +1,31 @@
+package com.example.jpa121.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "name cannot be Empty!")
+    @Column(columnDefinition = "varchar(20) not null")
+    private String name;
+
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id")
+    private Teacher teacher;
+
+}
